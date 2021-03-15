7b59affdb34142a0633817267f3545f4132599a6
Valeri-dragon <yakontekst01@yandex.ru>
2021-03-15 22:06:42 +0300

    feat: add the solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-
 exports.min = function min (array) {
-  return 0;
+    var massiv=Array.isArray(array);
+    var minItog=array[0];
+    var myarr=array.length;
+    if(!massiv||array===[]){
+     return 0, min();
+ }else{
+    for (var i=0; i<myarr; i++){
+        if (minItog> array[i]){
+         minItog= array[i];
+        }
+    }
+ 
+     return minItog;
+ }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+    var massiv=Array.isArray(array);
+    var itog=array[0];
+    var myarr=array.length;
+    if(!massiv||array===[]){
+        return 0, max();
+ }else{
+    for (var i=0; i<myarr; i++){
+        if (itog < array[i]){
+            itog= array[i];
+        }
+    }
+     return itog;
+ }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    var massiv=Array.isArray(array);
+    var myarr=array.length;
+    var s = 0;
+    if(!massiv||array===[]){
+        return 0, avg();
+  }else{
+ for (i = 0; i < myarr; i++){
+   
+    s += array[i];}
+var avgsItog=s/myarr;
+return avgsItog;
+ }
 }
